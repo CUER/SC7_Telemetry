facb625e2d069768b28b4edc61524ed9cb6704ee
Adi Jain <adi.jain1357@gmail.com>
2023-02-11 13:25:12 +0000

    Adi serial read into array

---
+++ SQlite/deployment/deployment.py
@@ -0,0 +1,28 @@
+import serial
+import threading
+import time
+ 
+arduino = serial.Serial(
+    port='COM6',
+    baudrate = 115200,
+    # parity=serial.PARITY_NONE,
+    # stopbits=serial.STOPBITS_ONE,
+    # bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+    
+print(f"Listening on port {arduino.port}")
+# while True:
+    # x = arduino.read()
+    # print(x)
+    
+while True:
+    x = arduino.readline()
+
+    # print(int.from_bytes(x, byteorder="little"))
+    x= x.split(",")
+    y=[]
+    for item in x:
+        y.append(int(item))
+
+
